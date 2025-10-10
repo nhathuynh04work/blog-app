ca6d4dd2cbc8549f70b4a9bc4c5991aa4bb0ac26
Anh Nhật <nhathuynh04work@gmail.com>
2025-10-10 10:51:51 +0700

    enable cors on backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? 4000);
+    const config = app.get(ConfigService);
+
+    app.enableCors({
+        origin: config.get<string>("FRONTEND_URL")?.split(",") || [
+            "http://localhost:3000",
+        ],
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    });
+
+    const port = config.get<number>("PORT") || 4000;
+    await app.listen(port);
 }
 bootstrap();
