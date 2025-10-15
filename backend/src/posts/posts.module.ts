c25a73f18bd8e90ded553d82a71d1db9bdc846f7
Anh Nhật <nhathuynh04work@gmail.com>
2025-10-15 10:14:27 +0700

    drop old likes collection index, fix import service bug

--- backend/src/posts/posts.module.ts
+++ backend/src/posts/posts.module.ts
@@ -3,10 +3,10 @@ import { PostsController } from "./posts.controller";
 import { PostsService } from "./posts.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Post } from "./entities/post.entity";
-import { LikesService } from "src/likes/likes.service";
+import { LikesModule } from "src/likes/likes.module";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Post]), LikesService],
+    imports: [TypeOrmModule.forFeature([Post]), LikesModule],
     controllers: [PostsController],
     providers: [PostsService],
     exports: [PostsService],
