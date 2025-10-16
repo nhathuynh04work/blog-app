8688e7e428dcac3b5e265279070a912cf19eb0c8
Anh Nhật <nhathuynh04work@gmail.com>
2025-10-16 10:17:53 +0700

    fix backend cookie config

--- backend/src/config/cookie.ts
+++ backend/src/config/cookie.ts
@@ -3,7 +3,7 @@ import { CookieOptions } from "express";
 export const cookieConfig: CookieOptions = {
     httpOnly: true,
     maxAge: 3600 * 1000,
-    sameSite: "lax",
-    domain: "localhost",
-    secure: false,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: process.env.NODE_ENV === "production",
+    domain: process.env.NODE_ENV === "production" ? undefined : "localhost",
 };
